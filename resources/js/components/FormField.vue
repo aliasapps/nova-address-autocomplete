a29b96adc278767aa70681755d1eb395a9172e37
Aaron Tagadtad <aaron@alias.com>
2021-04-29 12:56:38 -0400

    f

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,64 +1,71 @@
 <template>
-    <default-field :field="field">
-        <template slot="field">
-            <div class="form-group">
-                <vue-google-autocomplete
-                        ref="address"
-                        :id="field.name"
-                        class="w-full form-control form-input form-input-bordered"
-                        :class="errorClasses"
-                        :placeholder="field.name"
-                        :country="field.countries"
-                        v-on:placechanged="getAddressData">
-                </vue-google-autocomplete>
-            </div>
+  <default-field :field="field">
+    <template slot="field">
+      <div class="form-group">
+        <vue-google-autocomplete
+          ref="address"
+          :id="field.name"
+          class="w-full form-control form-input form-input-bordered"
+          :class="errorClasses"
+          :placeholder="field.name"
+          :country="field.countries"
+          v-on:placechanged="getAddressData"
+        >
+        </vue-google-autocomplete>
+      </div>
 
-            <p v-if="hasError" class="my-2 text-danger">
-                {{ firstError }}
-            </p>
-        </template>
-    </default-field>
+      <p v-if="hasError" class="my-2 text-danger">
+        {{ firstError }}
+      </p>
+    </template>
+  </default-field>
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import VueGoogleAutocomplete from 'vue-google-autocomplete'
+import { FormField, HandlesValidationErrors } from "laravel-nova";
+import VueGoogleAutocomplete from "vue-google-autocomplete";
 
 export default {
-    components: { VueGoogleAutocomplete },
+  components: { VueGoogleAutocomplete },
 
-    mixins: [FormField, HandlesValidationErrors],
+  mixins: [FormField, HandlesValidationErrors],
 
-    props: ['resourceName', 'resourceId', 'field'],
-    data: function () {
-        return {
-            address: ''
-        }
+  props: ["resourceName", "resourceId", "field"],
+
+  created() {
+    console.log("hai google address");
+    // Nova.$on(`${this.orderType}_order_type-change`, this.handleOrderType);
+  },
+
+  data: function() {
+    return {
+      address: "",
+    };
+  },
+  methods: {
+    getAddressData: function(addressData, placeResultData, id) {
+      this.handleChange(placeResultData.formatted_address);
     },
-    methods: {
-        getAddressData: function (addressData, placeResultData, id) {
-            this.handleChange(placeResultData.formatted_address)
-        },
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-          this.value = this.field.value || ''
-        },
+    /*
+     * Set the initial, internal value for the field.
+     */
+    setInitialValue() {
+      this.value = this.field.value || "";
+    },
 
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        fill(formData) {
-          formData.append(this.field.attribute, this.value || '')
-        },
+    /**
+     * Fill the given FormData object with the field's internal value.
+     */
+    fill(formData) {
+      formData.append(this.field.attribute, this.value || "");
+    },
 
-        /**
-         * Update the field's internal value.
-         */
-        handleChange(value) {
-          this.value = value
-        }
-    }
-}
+    /**
+     * Update the field's internal value.
+     */
+    handleChange(value) {
+      this.value = value;
+    },
+  },
+};
 </script>
